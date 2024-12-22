9aab9e240ebfb21128fb3ef8b71a598ef0029733
david.hsieh <>
2024-12-22 15:10:33 +0800

    [update] finished [list] finished [mark] finished

--- taskManager.py
+++ taskManager.py
@@ -4,60 +4,90 @@ from dataStructure import TaskID, TaskInfo
 
 task_id = 1
 
-def task_action(action, id, task):
+def task_action(action, id, task, TaskID):
     global task_id
-    TaskID[str(task_id)] = TaskInfo().__dict__
     
     if action == 'add':
+        TaskID[str(task_id)] = TaskInfo().__dict__
+
         TaskID[str(task_id)]['id'] = task_id
         TaskID[str(task_id)]['description'] = task
         TaskID[str(task_id)]['status'] = 'todo'
         TaskID[str(task_id)]['createdAt'] = TaskID[str(task_id)]['updatedAt'] = datetime.now().isoformat()
 
-
         write_data_to_json(action, id, TaskID)
         task_id += 1
     elif action == 'list':
-        displayList(action, TaskID, task)
+        # 確保 JSON 文件存在
+        data = ensure_task_file_exists()
+        if data is not None:
+            displayList(action, data, task)
+        else:
+            print("無法讀取 task.json。")
     else:
-        
-        write_data_to_json(action, id, task)
+        if action == 'update':
+            TaskID[id]['description'] = task
+            TaskID[id]['updatedAt'] = datetime.now().isoformat()
+        else:
+            TaskID[id]['updatedAt'] = datetime.now().isoformat()
+
+
+        write_data_to_json(action, id, TaskID)
 
 
 
 
 def main():
-    # 確保 JSON 文件存在
-    ensure_task_file_exists()
+    global task_id
+    try:
+        # 確保 JSON 文件存在
+        data = ensure_task_file_exists()
+        if data is not None:
+            orginal_file_data = data
+            task_id = len(data) + 1
+        id = None
 
-    id = None
+        while True:
+            user_input = input()
+            # 將輸入以空格為分隔符拆分
+            if "update" in user_input:
+                parts = user_input.split(' ', 2)
+                action = parts[0]
+                id = parts[1]
+                task = parts[2]
+            elif "list" in user_input:
+                parts = user_input.split(' ', 1)
+                if len(parts) > 1:
+                    action = parts[0]
+                    id = None
+                    task = parts[1]
+                else:
+                    action = parts[0]
+                    id = None
+                    task = None
+            elif "mark" in user_input:
+                parts = user_input.split(' ', 1)
+                action = parts[0]
+                id = parts[1]
+                task = None
+            else:
+                parts = user_input.split(' ', 1)
+                action = parts[0]
+                task = parts[1]
 
-    while True:
-        user_input = input()
-         # 將輸入以空格為分隔符拆分
-        if "update" in user_input:
-            parts = user_input.split(' ', 2)
-            action = parts[0]
-            id = parts[1]
-            task = parts[2]
-        else:
-            parts = user_input.split(' ', 1)
-            action = parts[0]
-            task = parts[1]
-
-        
-        if action == 'add':
-            task_action(action, id, task)
-        elif action == 'delete':
-            task_action(action, id, task)
-        elif action == 'update':
-            task_action(action, id, task)
-        elif action == 'mark-in-progress' or action == 'mark-todo' or action == 'mark-done':
-            task_action(action, id, task)
-        elif action == 'list':
-            task_action(action, id, task)
-
-
+            
+            if action == 'add':
+                task_action(action, id, task, orginal_file_data)
+            elif action == 'delete':
+                task_action(action, id, task, orginal_file_data)
+            elif action == 'update':
+                task_action(action, id, task, orginal_file_data)
+            elif action == 'mark-in-progress' or action == 'mark-todo' or action == 'mark-done':
+                task_action(action, id, task, orginal_file_data)
+            elif action == 'list':
+                task_action(action, id, task, orginal_file_data)
+    except ValueError as e:
+            print(f"錯誤：{e}。請重新輸入正確的選項！")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+from dataStructure import TaskID
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TASK_FILE = BASE_DIR + "/task.json"
@@ -31,17 +32,23 @@ def write_data_to_json(tag, id, new_data):
                 del existing_data[new_data]
         elif tag == 'update':
             # If existing_data is a dictionary, update the specified task
-            if isinstance(existing_data, dict) and new_data in existing_data:
-                existing_data[new_data]['description'] = new_data['description']
+            if isinstance(existing_data, dict) and int(id) in existing_data[id].values():
+                existing_data[id]['description'] = new_data[id]['description']
+                existing_data[id]['updatedAt'] = new_data[id]['updatedAt']
         elif tag == 'mark-in-progress':
-            if isinstance(existing_data, dict) and new_data in existing_data:
-                existing_data[new_data]['status'] = 'in-progress'
+            if isinstance(existing_data, dict):
+                existing_data[id]['status'] = 'in-progress'
+                existing_data[id]['updatedAt'] = new_data[id]['updatedAt']
         elif tag == 'mark-todo':
-            if isinstance(existing_data, dict) and new_data in existing_data:
-                existing_data[new_data]['status'] = 'todo'
+            if isinstance(existing_data, dict):
+                existing_data[id]['status'] = 'todo'
+                existing_data[id]['updatedAt'] = new_data[id]['updatedAt']
         elif tag == 'mark-done':
-            if isinstance(existing_data, dict) and new_data in existing_data:
-                existing_data[new_data]['status'] = 'done'
+            if isinstance(existing_data, dict):
+                existing_data[id]['status'] = 'done'
+                existing_data[id]['updatedAt'] = new_data[id]['updatedAt']
+        else:
+            raise ValueError(f"錯誤：{e}。請重新輸入正確的選項！")
         
         # Write the updated data back to the file
         with open(TASK_FILE, 'w') as file:
@@ -49,24 +56,27 @@ def write_data_to_json(tag, id, new_data):
         
         print("Data successfully written to data.json")
     
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except ValueError as e:
+        print(f"錯誤：{e}。請重新輸入正確的選項！")
+        
 
 
 def displayList(action, TaskDict , task):
     for key, value in TaskDict.items():
-        if value['status'] == "done" and action == "done":
+        if (value['status'] == task or task == None) and value['id'] is not None:
             print(TaskDict[key])
-        elif value['status'] == "todo" and action == "todo":
-            print(TaskDict[key])
-        elif value['status'] == "in-progress" and action == "in-progress":
-            print(TaskDict[key])
+       
 
 
 def ensure_task_file_exists():
+    data = TaskID
     """檢查 JSON 文件是否存在，如果不存在則創建一個空文件。"""
     if not os.path.exists(TASK_FILE):
         with open(TASK_FILE, "w") as f:
             json.dump({}, f)
         print(f"文件 {TASK_FILE} 已創建。")
+    else:
+        with open('task.json') as f:
+            data = json.load(f)
+    return data
 
